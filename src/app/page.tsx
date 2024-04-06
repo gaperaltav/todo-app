a1e1b4dce89e4fc1ddc5a248b178f3cecf6a57c4
Gabriel Peralta <gaperaltav@gmail.com>
2024-04-06 02:20:13 -0400

    adding something I forgot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { TodoList } from "./todo-list/page";
 
 export default function Home() {
--- src/app/todo-list/page.tsx
+++ src/app/todo-list/page.tsx
@@ -1,16 +1,16 @@
-'use client'
+"use client";
 
 import { checkTodo, createTodo, deleteTodo, fetchTodoList } from "@/db/actions";
 import { useEffect, useState } from "react";
 import TodoItem from "./todo-item";
 import { useCookies } from "react-cookie";
 
-export function TodoList(){
+export function TodoList() {
   const [todoText, setTodoText] = useState("");
   const [todos, setTodos] = useState<Todo[]>();
   const [loading, setLoading] = useState(true);
   const [cookie] = useCookies(["user_id"]);
-  const { user_id: userId } = cookie
+  const { user_id: userId } = cookie;
 
   const refetchTodoList = async () => {
     setLoading(true);
@@ -22,12 +22,12 @@ export function TodoList(){
 
   const checkTodoHandler = async (id: number, value: boolean) => {
     setLoading(true);
-   checkTodo(id, value).then(() => refetchTodoList())
-  }
+    checkTodo(id, value).then(() => refetchTodoList());
+  };
 
   const deleteTodoHandler = async (id: number) => {
-   deleteTodo(id).then(() => refetchTodoList())
-  }
+    deleteTodo(id).then(() => refetchTodoList());
+  };
 
   const addTodoHandler = async () =>
     createTodo(todoText, userId).then(() => {
@@ -36,46 +36,48 @@ export function TodoList(){
     });
 
   useEffect(() => {
-    refetchTodoList()
+    refetchTodoList();
   }, []);
 
-    return(<div className="flex w-[100%] justify-center ">
-    <div className="flex flex-col">
-      <h1 className="text-lg">Todo app</h1>
-      <div className="add-todo-text">
-        <input
-          type="text"
-          className="h-[30px] w-[350px]"
-          placeholder="TODO text"
-          value={todoText}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            setTodoText(event.target.value)
-          }
-        />
-        <button
-          type="button"
-          onClick={addTodoHandler}
-          className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mx-1 disabled:bg-gray-300 disabled:cursor-not-allowed`}
-          disabled={todoText.trim() === ""}
-        >
-          Add
-        </button>
-      </div>
-      <div className="todo-list">
-        {loading && <div>Loading...</div>}
-        {!loading && todos && (
-          <ul>
-            {todos.map((todo, i) => (
-              <TodoItem
-                key={i}
-                data={todo}
-                onCheckTodo={(id, value) => checkTodoHandler(id, value)}
-                onDeleteTodo={(id) => deleteTodoHandler(id)}
-              />
-            ))}
-          </ul>
-        )}
+  return (
+    <div className="flex w-[100%] justify-center ">
+      <div className="flex flex-col">
+        <h1 className="text-lg">Todo app</h1>
+        <div className="add-todo-text">
+          <input
+            type="text"
+            className="h-[30px] w-[350px]"
+            placeholder="TODO text"
+            value={todoText}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+              setTodoText(event.target.value)
+            }
+          />
+          <button
+            type="button"
+            onClick={addTodoHandler}
+            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mx-1 disabled:bg-gray-300 disabled:cursor-not-allowed`}
+            disabled={todoText.trim() === ""}
+          >
+            Add
+          </button>
+        </div>
+        <div className="todo-list">
+          {loading && <div>Loading...</div>}
+          {!loading && todos && (
+            <ul>
+              {todos.map((todo, i) => (
+                <TodoItem
+                  key={i}
+                  data={todo}
+                  onCheckTodo={(id, value) => checkTodoHandler(id, value)}
+                  onDeleteTodo={(id) => deleteTodoHandler(id)}
+                />
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
     </div>
-  </div>)
+  );
 }
